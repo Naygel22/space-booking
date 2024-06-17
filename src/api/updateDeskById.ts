a514d6fd6e8cef64102a7418f0868ceb7dd25850
Naygel22 <piotrkozlowski14@gmail.com>
2024-06-17 17:08:02 +0200

    link in updatedeskbyid corrected

--- src/api/updateDeskById.ts
+++ src/api/updateDeskById.ts
@@ -1,7 +1,7 @@
 export type UpdateDeskType = { newStatus: boolean, deskId: string }
 
 export const updateDeskById = async ({ newStatus, deskId }: UpdateDeskType) => {
-  const response = await fetch(`http://localhost:3000/invoices/${deskId}`, {
+  const response = await fetch(`http://localhost:3000/desks/${deskId}`, {
     method: "PATCH",
     headers: { "Content-type": "application/json;charset=UTF-8" },
     body: JSON.stringify({ available: newStatus })
