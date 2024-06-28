13506d85834be8cad29c875b6816df395ed3f45a
Naygel22 <piotrkozlowski14@gmail.com>
2024-06-28 18:22:41 +0200

    google and github log in buttons

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -2,16 +2,24 @@ import { Route, Routes } from "react-router-dom"
 import { RegisterPage } from "../pages/RegisterPage"
 import { SpaceViewer } from "./SpaceViewer"
 import { LoginPage } from "../pages/LoginPage"
+import { useSessionContext } from "./SessionProvider"
 
 export const Browse = () => {
+  const { session } = useSessionContext()
+
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>} />
       <Route path="/booking" element={<SpaceViewer />} />
       <Route path='/pricing' element={<div>Pricing</div>} />
       <Route path='/contact' element={<div>Contact</div>} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/login' element={<LoginPage />} />
+      {!session &&
+        <>
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </>
+      }
+
     </Routes>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -20,6 +20,19 @@ export const Login = () => {
       <TextInput formik={formik} accessor='mail' label='E-mail' />
       <TextInput formik={formik} accessor='password' label='Password' type='password' />
       <button type="submit" className="loginButton">Log In</button>
+      <button type="button" className="loginWithGoogleButton">
+        <img src="src/assets/images/googleLogo.png" className="googleLogo" />
+        Log in with Google
+      </button>
+      <button type="button" className="loginWithGithubButton">
+        <img src="src/assets/images/githubLogo.png" className="githubLogo" />
+        Log in with GitHub
+      </button>
+
+      <div>
+        <div className="signupButtonInLoginForm">New? Sign up and book your desk!</div>
+      </div>
     </form>
+
   )
 }
